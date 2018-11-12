d653d4a79326f84eb59d2fc9fe1d6f3aeadf49a2
Arnaldo Flores <nwtech23@gmail.com>
2018-11-11 23:17:14 -0500

    Added tabindex and aria label to main parts of the pages

--- js/main.js
+++ js/main.js
@@ -159,6 +159,7 @@ createRestaurantHTML = (restaurant) => {
   const li = document.createElement('li');
 
   const image = document.createElement('img');
+  image.alt = restaurant.name + "restaurant image"
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
@@ -177,6 +178,7 @@ createRestaurantHTML = (restaurant) => {
 
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
+  more.tabIndex = '4';
   more.href = DBHelper.urlForRestaurant(restaurant);
   li.append(more)
 
