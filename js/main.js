0ca09730bb2701fcbac8503d4210011f247d037b
Arnaldo Flores <nwtech23@gmail.com>
2018-11-12 06:50:41 -0500

    Added space on img alt  text for readability

--- js/main.js
+++ js/main.js
@@ -159,7 +159,7 @@ createRestaurantHTML = (restaurant) => {
   const li = document.createElement('li');
 
   const image = document.createElement('img');
-  image.alt = restaurant.name + "restaurant image"
+  image.alt = restaurant.name + " restaurant image"
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
